dba2d71aaf5775fad32cc806992cd86d60aa408a
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2020-05-12 16:37:28 -0400

    require google apis differently

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const {googleapis} = require('googleapis');
 
 var climateMessage = "This is the monthly climate coach report, here to give you an \
   overview of various metrics in this repository, such as responsiveness and tone used in discussions"; 
 
 
-
-var API_KEY = 'AIzaSyBWTRG1IogLfBoqOd5DYdlWbBr5ctvkTJo'
 var DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1'
 
 function run() {
+  const {google} = require("googleapis");
+
   var client = new github.GitHub(core.getInput('repo-token', { required: true }));
   const API_KEY = core.getInput('google-api-key');
   //const repoName = core.getInput('repo-name');
@@ -24,7 +23,7 @@ function run() {
   //     body: climateMessage
   // });
 
-  commentAnalyzer = googleapis.commentanalyzer('v1alpha1');
+  commentAnalyzer = google.commentanalyzer('v1alpha1');
 
   var analyzeRequest = {
     comment: {text: 'what kind of idiot name is foo?'},
