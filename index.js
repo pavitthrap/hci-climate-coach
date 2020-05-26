65e3bf5791ad5e3dc110a176f623df9eff6e38ac
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2020-05-26 16:37:03 -0400

    sample texts

--- index.js
+++ index.js
@@ -76,19 +76,13 @@ function updateToxicityInMap(toxicity, user, ID, text, toxicityScores) {
 function cleanText(text) {
   // remove code snippets
   const regex = /(```.+?```)/;
-  console.log("before replace");
   var next = text.replace(regex, ''); 
-  console.log("next: ", next);
   
   while (next != text) {
-    console.log("replacing again... text is: ", text);
     text = next;
-    console.log("before replace");
     var next = text.replace(regex, ''); 
-    console.log("next: ", next);
   }
 
-  console.log("before stripping md, next: ", next, ", text: ", text);
   // remove markdown formatting 
   var plainText = removeMd(text); 
   return plainText; 
@@ -184,10 +178,8 @@ function getToxicityScores(client, owner, repo, commentAnalyzer, toxicityScoresI
 
 
 // TODO - clean input 
-//   remove code blocks '''
-//   other irrelevant bits (like urls, )
-//   look for markdown parser 
-//   https://github.com/markedjs/marked 
+//   [x] remove code blocks '''
+//   [ ] should I remove blockquotes? typically refers to others' comments   
 
 // TODO - run it on moderation examples post input pruning 
 //     - try running sentence by sentence 
@@ -219,10 +211,72 @@ function run() {
       console.log("Proportion of comments exceeding toxicity threshold: ", numOverThreshold/numSamples); 
     }
     
+    //const markdown = ' also ``` here is code``` \n# This is a heading\n\nThis is ``` code pt 2``` a paragraph with [a link](http://www.disney.com/) in it.';
+    var texts = ["WIP: Sent all girls to factory This PR will make sure that the factories provided by alchemy_cms can be used in our own tests host application's without using a special procedure before (like the Alchemy::Seeder).",
+        "@julienben @gretzky Examples or you're wrong.", "Guess you're wrong.", "Nah, you're just rude. Your way is not the 'right' way.", "Those Indian developers in Google really did something wrong with Google Console product", 
+        "So this issue is like a half a year old, any improvements? the whole shadow DOM thing is utter crap, it adds more headaches than it solves, because we're unable to solve things on our own without waiting for you guys to move your lazy butts and add the variable.",
+        "Ionic version: (check one with 'x')\
+[x ] 3.x\
+\
+I'm submitting a ... (check one with 'x')\
+[x] bug report\
+\
+Related code:\
+```\
+    showToast($event) {\
+        this.toastController.create({message: 'fuck', position: 'middle'}).present();\
+    }\
+```\
+",      
+        "WHY THE FUCK AM I GETTING THESE MAILS ARE YOU FUICKING RETARDED?????",
+         "Hey @TomasKatz , while I agree that the email notifications can get a little frustrating sometimes, we do ask that you follow our code of conduct at all times. Also, since this issue has been resolved I am going to lock this issue for now.",
+         "Lol makes no sense. Why would it be because of microsoft ? the app handles the bring to front not microsoft lol",
+         "It is same on web version. Try it. Maybe @klaussinani can work around fix it. But dont make fun of people, u violate code of conduct https://github.com/klaussinani/ao/blob/master/code-of-conduct.md \
+      Also mayde considerered contributting with PR",
+      "@caishengmao Please stop raising duplicate issues, please follow up in your original issue #586 \
+  \
+      Further duplicates will be considered (at least in my opinion) against the Electron Community CoC.",
+      "Yesterday afternoon, I started getting this message on a project that I was working on in the morning without incident. Nothing changed except the time. \
+\
+I was able to eliminate it by reducing the scope of the watched list but that meant I was not getting proper coverage.\
+\
+I reinstalled and otherwise tried things. Eventually I got it to go away by upgrade from node 10 LTS to node 11.1.0.\
+\
+This is just stupid.",
+      "> This is just stupid.\
+\
+      ☝️ I was referring to this part of your comment. Language like this will do nothing to motivate contributors to prioritize this issue. If anything, it will disincentivize people to look into solving it. Please familiarize yourself with the Node.js Code of Conduct to have a better sense of what kinds of behavior are (not) tolerated in this project.",
+      "Que mamones, si no van a ayudar mejor ni comenten :D",
+      "Get rid of that idiot zloirock and his core-js library Is your feature request related to a problem? Please describe.\
+When npm install is executed, the terminal is full of messages like:\
+```\
+Thank you for using core-js ( https://github.com/zloirock/core-js ) for polyfilling JavaScript standard library!\
+\
+The project needs your help! Please consider supporting of core-js on Open Collective or Patreon:\
+> https://opencollective.com/core-js\
+> https://www.patreon.com/zloirock\
+\
+Also, the author of core-js ( https://github.com/zloirock ) is looking for a good job -)\
+```\
+This is a major problem in the open source community, that we may depend on work of someone who might as well go nuts at some moment and we are at his mercy. There has been a big discussion here zloirock/core-js#635 where the author is openly reckless and promotes the freedom to clog his own library any way he wants. It is also being discussed around the internet.\
+\
+Describe the solution you'd like\
+Now that it has been identified, I believe major libraries should break apart from this library, and replace its functionality with something else.\
+\
+Are you able to assist bring the feature to reality?\
+I would be happy to try, unless there is someone capable of doing it much faster than me."
+              ]
 
-    const markdown = ' also ``` here is code``` # This is a heading\n\nThis is ``` code pt 2``` a paragraph with [a link](http://www.disney.com/) in it.';
-    var cleaned = cleanText(markdown);
-    console.log("cleaned text: ", cleaned);
+    for (var i = 0; i < texts.length; i++) {
+      var text = texts[i]; 
+      var cleaned = cleanText(markdown);
+      var tox1 = analyzeToxicity(commentAnalyzer, text);
+      var tox2 = analyzeToxicity(commentAnalyzer, cleaned);
+      console.log("on text number ", i, " : ", text);
+      console.log("CLEANED text: ", cleaned);
+      console.log("TOX prev: ", tox1, ", TOX clean: ", tox2); 
+    }
+   
 
     // TODO - maybe apply some filtering to the toxicity scores?
     //  [x] apply threshold 
